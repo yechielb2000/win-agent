79897740efa3f66d9c96c05edb203ff430905c3b
Yechiel Babani <yechielb2000@gmail.com>
2025-05-25 09:57:49 +0300

    put ProcessInfo in common

---
+++ agent/common/processInfo.hpp
@@ -0,0 +1,15 @@
+#ifndef PROCESSINFO_HPP
+#define PROCESSINFO_HPP
+
+#pragma once
+#include <iostream>
+
+
+struct ProcessInfo {
+    std::string name;
+    std::string path;
+    std::string hash256;
+};
+
+
+#endif
--- agent/configFileSyner/configFile.hpp
+++ agent/configFileSyner/configFile.hpp
@@ -1,17 +1,8 @@
-//
-// Created by yechi on 5/25/2025.
-//
-
 #ifndef CONFIGFILE_HPP
 #define CONFIGFILE_HPP
 #include <string>
 
 
-struct ProcessInfo {
-    std::string name;
-    std::string path;
-    std::string hash256;
-};
 
 struct configFile {
 
